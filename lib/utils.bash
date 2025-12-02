4ffd3d8cac881a3b525a016e45dc1840612244b1
Dobes Vandermeer <dobes@formative.com>
2025-12-01 16:28:20 -0800

    Only support v4 or later

--- lib/utils.bash
+++ lib/utils.bash
@@ -25,7 +25,8 @@ sort_versions() {
 list_github_tags() {
 	git ls-remote --tags --refs "$GH_REPO" |
 		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+		sed 's/^v//' | # NOTE: You might want to adapt this sed to remove non-version strings from tags
+		awk -F. '$1 + 0 >= 4'
 }
 
 list_all_versions() {
